e68e1245b65a27237f3470b4fb9307c186b73ee6
Giorgi Rapava <grapava00@gmail.com>
2022-12-08 05:18:17 +0400

    add mobile and tablet styles for light mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import './App.css'
+import moon from './assets/moon.png'
+import search from './assets/search.png'
+import logo from './assets/logo.png'
+import company from './assets/company.svg'
+import location from './assets/location.svg'
+import twitter from './assets/twitter.svg'
+import website from './assets/website.svg'
 
 
 export default function App(){
-    // const [input, setInput] = React.useState('')
-    const [data, setData] = React.useState('')
 
-    const fetchData = () =>{
-        axios.get("https://api.github.com/users/grapava00")
-        .then((res)=>{setData(res.data)})
-    }
+    // const [user, setUser] = React.useState('')
+    // const [data, setData] = React.useState('')
 
-    useEffect(()=>{
-        fetchData()
-    },[])
+    // function handleChane(event){
+    //     setUser(event.target.value)
+    // }
+
+    // const fetchData = () =>{
+    //     if(user){
+    //         axios.get("https://api.github.com/users/"+user)
+    //         .then((res)=>{setData(res.data)})
+    //     }
+    // }
+
+    // useEffect(()=>{
+    //     fetchData()
+    // },[])
 
 
     return(
      <>
-       <button onClick={fetchData}>fetch</button>
-       {data.created_at}
+      <header className='flex justify-between pt-8 mb-9 sm:bg-black mt-35'>
+        <p className='text-2xl text-infinity-700'>devfinder</p>
+        <div className='flex items-center'>
+            <p className='mr-4 text-sm text-san-marino-700 '>dark</p>
+            <img src={moon} alt='moon icon'/>
+        </div>
+      </header>
+      <main>
+        <form className='relative'>
+          <img src={search} alt='search-icon'className='absolute top-5 left-4 left-8'/>
+          <input className='w-full h-15 rounded-2xl border-none outline-none bg-white-as-heaven indent-11 text-san-marino-400 text-sm caret-blue-sparkle text-lg indent-20' placeholder='Search GitHub username…' />
+          <button className='absolute top-3 right-4 py-3 px-4 border-none rounded-xl bg-blue-sparkle text-white-700'>Search</button>
+        </form>
+        <section className='mt-4 pt-8 pb-12 px-6 bg-white-as-heaven rounded-2xl px-10 py-10'>
+          <div className='flex flex-wrap'>
+            <img className='w-17 mr-4 mr-10 w-28' src={logo} alt='github logo'/>
+            <div>
+              <p className='text-anchors-aweigh-700 text-base mb-1 text-2xl'>The Octocat</p>
+              <p className='text-blue-sparkle-400 text-sm mb-1 text-base'>@octocat</p>
+              <p className='text-searching-blue-400 text-sm text-xs'>Joined 25 Jan 2011</p>
+            </div>
+            <p className='mt-8 text-san-marino-400'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.</p>
+          </div>
+          <div className='flex justify-between px-3 py-5 mt-6 bg-ghost-white rounded-xl'>
+            <div>
+              <p className='text-san-marino-400 text-xs'>Repos</p>
+              <p className='text-center text-anchors-aweigh-700 mt-2 text-base'>8</p>
+            </div>
+            <div>
+              <p className='text-san-marino-400 text-xs'>Followers</p>
+              <p className='text-center text-anchors-aweigh-700 mt-2 text-base'>3938</p>
+            </div>
+            <div>
+              <p className='text-san-marino-400 text-xs'>Following</p>
+              <p className='text-center text-anchors-aweigh-700 mt-2 text-base'>9</p>
+            </div>
+          </div>
+          <div className='flex flex-col mt-6 flex-row'>
+            <div className='mr-15'>
+              <div className='flex mb-4'>
+                <img className='mr-4' src={location} alt='location icon'/>
+                <p className='text-sm text-san-marino-400'>San Francisco</p>
+              </div>
+              <div className='flex mb-4'>
+                <img className='mr-4' src={website} alt='location icon'/>
+                <p className='text-sm text-san-marino-400'>https://github.blog</p>
+              </div>
+            </div>
+            <div>
+              <div className='flex mb-4'>
+                <img className='mr-4' src={twitter} alt='location icon'/>
+                <p className='text-sm text-san-marino-400 opacity-80'>Not Available</p>
+              </div>
+              <div className='flex'>
+                <img className='mr-4' src={company} alt='location icon'/>
+                <p className='text-sm text-san-marino-400'>@github</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
     )
 }
